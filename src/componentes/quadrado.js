a85aa0b6ccdfbbf4b640ab80d2a59f4844d66752
Eduardo Maia Fontainha <eduardo.maia@estudante.ufjf.br>
2026-06-26 17:45:39 -0300

    :recycle: Refactor: Estrutura do componente quadrado e na lógica de renderização

--- src/componentes/quadrado.js
+++ src/componentes/quadrado.js
@@ -1,35 +1,79 @@
 export default class Quadrado extends HTMLElement {
+  #estado = {
+    linha: null,
+    coluna: null,
+    mina: false,
+    revelado: false,
+  };
+
   constructor() {
     super();
 
-    const shadow = this.attachShadow({ mode: "open" });
+    this.attachShadow({ mode: "open" });
+  }
 
-    const estilo = document.createElement("style");
+  static get observedAttributes() {
+    return ["linha", "coluna", "mina"];
+  }
 
-    estilo.textContent = `
-    div{
-        width:50px;
-        height:50px;
-        background:red;
-        color:white;
-        display:flex;
-        align-items:center;
-        justify-content:center;
-        cursor:pointer;
-    }`;
+  connectedCallback() {
+    this.#render();
+  }
 
-    const quadrado = document.createElement("div");
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === "mina") {
+      this.#estado.mina = newValue === "true";
+    }
 
-    quadrado.textContent = "";
+    if (name === "linha") {
+      this.#estado.linha = Number(newValue);
+    }
 
-    quadrado.addEventListener("click", () => {
-        
-      quadrado.textContent = "0";
+    if (name === "coluna") {
+      this.#estado.coluna = Number(newValue);
+    }
+  }
+
+  #render() {
+    const shadow = this.shadowRoot;
+
+    shadow.innerHTML = `
+      <style>
+        div{
+          width:50px;
+          height:50px;
+          background:red;
+          display:flex;
+          align-items:center;
+          justify-content:center;
+          cursor:pointer;
+        }
+
+        .mina{
+          background:black;
+        }
+      </style>
+
+      <div></div>
+    `;
+
+    const div = shadow.querySelector("div");
+
+    div.addEventListener("click", () => {
+      this.dispatchEvent(
+        new CustomEvent("clicou", {
+          detail: {
+            linha: this.#estado.linha,
+            coluna: this.#estado.coluna,
+          },
+          bubbles: true,
+        }),
+      );
     });
 
-    shadow.appendChild(estilo);
-
-    shadow.appendChild(quadrado);
+    if (this.#estado.mina) {
+      div.classList.add("mina");
+    }
   }
 }
 
