f62db5afa86fcd76137c474b1128f0a82a53afba
Eduardo Maia Fontainha <eduardo.maia@estudante.ufjf.br>
2026-06-26 14:24:26 -0300

    :sparkles:feat: Orquestrador

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import "./componentes/Tabela.js";
+import "./componentes/quadrado.js";
+
+import {CampoMinado} from "./logica/jogo.js";
+
+
+const jogo = new CampoMinado(10,10);
+
+
+jogo.implementarMinas(20);
+
+
+const tabela=document.querySelector("layout-campo");
+
+
+tabela.mostrarJogo(
+    jogo.pegarTabuleiro(),
+    console.log(jogo)
+);
