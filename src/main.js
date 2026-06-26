7994d9779e19a7b3fedecba3f276844651a20ec4
Eduardo Maia Fontainha <eduardo.maia@estudante.ufjf.br>
2026-06-26 17:49:19 -0300

    :broom: cleanup: Ajuste de formatação

--- src/main.js
+++ src/main.js
@@ -1,19 +1,14 @@
 import "./componentes/Tabela.js";
 import "./componentes/quadrado.js";
 
-import {CampoMinado} from "./logica/jogo.js";
+import { CampoMinado } from "./logica/jogo.js";
 
-
-const jogo = new CampoMinado(10,10);
-
+const jogo = new CampoMinado(10, 10);
 
 jogo.implementarMinas(20);
 
+const tabela = document.querySelector("layout-campo");
 
-const tabela=document.querySelector("layout-campo");
+tabela.iniciar(jogo);
 
-
-tabela.mostrarJogo(
-    jogo.pegarTabuleiro(),
-    console.log(jogo)
-);
+console.log(jogo);
