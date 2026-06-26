3a3323d621f237cbb6800034b95321a5e570f971
Eduardo Maia Fontainha <eduardo.maia@estudante.ufjf.br>
2026-06-26 17:46:51 -0300

    :recycle: Refactor: Simplificando componente tabela e a logica de renderização

--- src/componentes/Tabela.js
+++ src/componentes/Tabela.js
@@ -1,48 +1,68 @@
-import Quadrado from "./quadrado.js";
-
 export default class Tabela extends HTMLElement {
+
     constructor() {
         super();
 
-        const shadow = this.attachShadow({ mode: "open" });
+        this.jogo = null;
 
-        let linhas = 10;
-        let colunas = 10;
+        const shadow = this.attachShadow({ mode: "open" });
 
         const estilo = document.createElement("style");
 
         estilo.textContent = `
-
-        :host{
-
-            display:grid;
-
-            grid-template-columns:repeat(${colunas},50px);
-
-            gap:2px;
-
+        :host {
+            display: grid;
+            grid-template-columns: repeat(10, 50px);
+            gap: 2px;
         }
-
         `;
 
         shadow.appendChild(estilo);
+    }
+
+    iniciar(jogo) {
+        this.jogo = jogo;
+        this.render();
+    }
+
+    render() {
+
+        const shadow = this.shadowRoot;
+        shadow.innerHTML += "";
+
+        const tabuleiro = this.jogo.pegarTabuleiro();
+
+        tabuleiro.forEach((linha, i) => {
+            linha.forEach((casa, j) => {
+
+                const quadrado = document.createElement("campo-quadrado");
+
+                quadrado.setAttribute("linha", i);
+                quadrado.setAttribute("coluna", j);
+
+                if (casa.mina) {
+                    quadrado.setAttribute("mina", "true");
+                }
+
+                shadow.appendChild(quadrado);
+            });
+        });
 
     }
-        mostrarJogo(tabuleiro) {
-            const shadow = this.shadowRoot;
 
-            tabuleiro.forEach((linha) => {
-                linha.forEach((casa) => {
-                    const quadrado = document.createElement("campo-quadrado");
+    connectedCallback() {
 
-                    if (casa.mina) {
-                        quadrado.setAttribute("mina", "true");
-                    }
+        this.addEventListener("clicou", (e) => {
 
-                    shadow.appendChild(quadrado);
-                });
-            });
-        }
+            const { linha, coluna } = e.detail;
+
+            const resultado = this.jogo.cliqueMinas(linha, coluna);
+
+            console.log("Resultado:", resultado);
+
+        });
+
+    }
 }
 
 customElements.define("layout-campo", Tabela);
