3f24bff433dcdd09d0f14e87df93f5e75e500465
Eduardo Maia Fontainha <eduardo.maia@estudante.ufjf.br>
2026-07-22 17:57:39 -0300

    :recycle: Refactor: Melhora a lógica de clique no componente Tabela e adiciona alerta de perda

--- src/componentes/Tabela.js
+++ src/componentes/Tabela.js
@@ -1,5 +1,4 @@
 export default class Tabela extends HTMLElement {
-
     constructor() {
         super();
 
@@ -26,7 +25,6 @@ export default class Tabela extends HTMLElement {
     }
 
     render() {
-
         const shadow = this.shadowRoot;
         shadow.innerHTML += "";
 
@@ -34,7 +32,6 @@ export default class Tabela extends HTMLElement {
 
         tabuleiro.forEach((linha, i) => {
             linha.forEach((casa, j) => {
-
                 const quadrado = document.createElement("campo-quadrado");
 
                 quadrado.setAttribute("linha", i);
@@ -47,21 +44,34 @@ export default class Tabela extends HTMLElement {
                 shadow.appendChild(quadrado);
             });
         });
-
     }
 
     connectedCallback() {
+        this.addEventListener(
+            "clicou",
+            (e) => {
+                console.log("Este código só roda no PRIMEIRO clique!");
+                
+                this.addEventListener("clicou", (eventoPosterior) => {
+                    const { linha, coluna } = eventoPosterior.detail;
+                    console.log(
+                        "Clique recebido (A partir do segundo):",
+                        eventoPosterior.detail,
+                    );
 
-        this.addEventListener("clicou", (e) => {
+                    const quadradoRevelado = this.jogo.quadradoRevelado(linha, coluna);
+                    console.log("Quadrado revelado:", quadradoRevelado);
 
-            const { linha, coluna } = e.detail;
+                    const resultado = this.jogo.cliqueMinas(linha, coluna);
+                    console.log("Resultado:", resultado);
 
-            const resultado = this.jogo.cliqueMinas(linha, coluna);
-
-            console.log("Resultado:", resultado);
-
-        });
-
+                    if (resultado === "perdeu") {
+                        alert("Você perdeu!");
+                    }
+                });
+            },
+            { once: true },
+        ); // O '{ once: true }' remove este primeiro ouvinte imediatamente
     }
 }
 
